481e49548f02c5123a30e91bad784b6ac940d474
Ronaldo <ronaldoarg@gmail.com>
2021-01-22 19:52:23 -0300

    logo and background animated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Melhor treinamento de Docker do Brasil
+          Melhor treinamento de Docker do Brasil <br /> Agora com tudo animado <br /> Sejam bem vindos e vindas stags \o/
         </p>
       </header>
     </div>
