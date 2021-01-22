e1e7153622c7515e78dc5840019d47b0bf206719
Ronaldo <ronaldoarg@gmail.com>
2021-01-22 19:06:46 -0300

    init

--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>. Dockerized React App.
+          Melhor treinamento de Docker do Brasil
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
